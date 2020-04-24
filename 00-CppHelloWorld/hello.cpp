f8883523b9c123a74749e2ba797539e64639ca4a
gmacedarodriguez <63563808+gmacedarodriguez@users.noreply.github.com>
2020-04-23 21:09:20 -0300

    Update hello.cpp
    Cambio el codigo para que sea más simple

--- 00-CppHelloWorld/hello.cpp
+++ 00-CppHelloWorld/hello.cpp
@@ -4,18 +4,8 @@
   23/04/2020 */
 
 #include<iostream>
-#include <fstream>
-#include <string>
-
-using namespace std;
 
 int main()
 {
-    string hola = "Hello, world!";
-    ofstream file;
-    file.open("output.txt");
-    file << hola << endl;
-    file.close();
-
-  return 0;
+std::cout << "Hello, world!"
 }
